33382cb3e7124c7900fd2d47d6d0d3b13b9f1da8
Danilo Pianini <danilo.pianini@gmail.com>
2020-03-09 22:46:34 +0100

    Fix dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.eclipse.mylyn.github:org.eclipse.egit.github.core:+")
-    implementation("org.eclipse.jgit:org.eclipse.jgit:+")
-    implementation("com.uchuhimo:konf:+")
+    implementation("org.eclipse.mylyn.github:org.eclipse.egit.github.core:2.1.5")
+//    implementation("org.eclipse.jgit:org.eclipse.jgit:+")
+    implementation("com.uchuhimo:konf:0.22.1")
     implementation("io.github.classgraph:classgraph:4.8.65")
     implementation("io.arrow-kt:arrow-core:0.10.4")
     implementation(kotlin("stdlib-jdk8"))
