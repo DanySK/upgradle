d8f5b3fa0b3b1343744f8dcc82c4731fba667c58
Danilo Pianini <danilo.pianini@gmail.com>
2020-03-10 00:07:16 +0100

    Add semver and publish on central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,19 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    `maven-publish`
     kotlin("jvm") version "1.3.70"
+    id("org.danilopianini.git-sensitive-semantic-versioning") version "0.2.2"
+    id("org.danilopianini.publish-on-central") version "0.2.3"
+    id("org.jetbrains.dokka") version "0.10.1"
 }
 
 repositories {
     mavenCentral()
+}
+
+gitSemVer {
+    version = computeGitSemVer()
 }
 
 dependencies {
@@ -24,4 +32,12 @@ compileKotlin.kotlinOptions {
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
     jvmTarget = "1.8"
+}
+
+group = "org.danilopianini" // This must be configured for the generated pom.xml to work correctly
+publishOnCentral {
+    projectDescription.set("A bot meant to pack updates and send pull requests. Much like dependabot, but way more homemade and focused on Gradle.")
+    projectLongName.set("UpGradle")
+    licenseName.set("GPL")
+    licenseUrl.set("https://www.gnu.org/licenses/gpl-3.0.en.html")
 }
