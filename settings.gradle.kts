173dc29e9c872dd92c8a24ab2ec982937212e6ec
Danilo Pianini <danilo.pianini@gmail.com>
2020-03-26 19:36:38 +0100

    Enable support for scans on CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,15 @@ buildscript {
 }
 
 bootstrapRefreshVersionsAndDependencies()
+
+
+plugins {
+    id("com.gradle.enterprise") version "3.2"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
