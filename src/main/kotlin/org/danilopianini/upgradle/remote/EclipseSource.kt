5ada8870db524916a568cd85bcfce15d86076336
Johannes Koenen <joekoe92@gmail.com>
2020-05-23 15:31:21 +0200

    Implement new branch sources on interfaces

--- src/main/kotlin/org/danilopianini/upgradle/remote/EclipseSource.kt
+++ src/main/kotlin/org/danilopianini/upgradle/remote/EclipseSource.kt
@@ -6,25 +6,31 @@ import org.eclipse.egit.github.core.Repository as GRepository
 import org.eclipse.egit.github.core.RepositoryBranch
 import org.eclipse.egit.github.core.service.RepositoryService
 import org.danilopianini.upgradle.remote.BranchSource.SelectedRemoteBranch
+import org.slf4j.LoggerFactory
 import java.util.stream.Collectors
+import java.util.stream.Stream
 
-class EclipseGitSource(private val service: RepositoryService) : BranchSource {
-    override fun getMatching(includes: Set<RepoDescriptor>, excludes: Set<RepoDescriptor>): Set<SelectedRemoteBranch> =
+class EclipseSource(private val service: RepositoryService) : BranchSource {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    override fun getMatching(selector: Selector): Set<SelectedRemoteBranch> =
         service.repositories.parallelStream()
             .map(::EGitRepository)
-            .filter { repo ->
-                includes.any { it matches repo } && excludes.none { it matches repo }
-            }
+            .onEach { logger.info("Found ${it.generateId()}") }
+            .filter { repo -> selector.applyTo(repo, RepoDescriptor::matches) }
             .flatMap { repo ->
                 service.getBranches(repo).parallelStream()
                     .map(::EGitBranch)
-                    .filter { branch ->
-                        includes.any { it matches branch } && excludes.none { it matches branch }
-                    }
+                    .filter { branch -> selector.applyTo(branch, RepoDescriptor::matches) }
                     .map { SelectedRemoteBranch(repo, it) }
             }
-            .collect(Collectors.toSet())
+            .collect(Collectors.toSet()).also {
+                logger.info("Found ${it.size} matching repositories.")
+            }
 }
+
+private inline fun <T>Stream<T>.onEach(crossinline f: (T) -> Unit): Stream<T> =
+    map { it.also(f) }
 
 private inline class EGitBranch(private val underlying: RepositoryBranch) : Branch {
     override val name: String get() = underlying.name
