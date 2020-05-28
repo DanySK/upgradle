109f81d1c3d114b79578743e5e026210e0931dcc
Johannes Koenen <joekoe92@gmail.com>
2020-05-28 21:58:37 +0200

    Set max_line_length to 120 and adapt files

--- src/test/kotlin/org/danilopianini/upgradle/modules/TestGradleWrapper.kt
+++ src/test/kotlin/org/danilopianini/upgradle/modules/TestGradleWrapper.kt
@@ -3,8 +3,6 @@ package org.danilopianini.upgradle.modules
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldNotBe
-import org.danilopianini.upgradle.modules.GradleVersion
-import org.danilopianini.upgradle.modules.GradleWrapper
 import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
