0882bb2b9856c1dfed2829c5ca634e0165c8f414
Danilo Pianini <danilo.pianini@gmail.com>
2021-05-31 17:56:42 +0200

    Update refreshVersions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,20 @@
-import de.fayard.refreshVersions.bootstrapRefreshVersions
-import de.fayard.refreshVersions.migrateRefreshVersionsIfNeeded
 import org.danilopianini.VersionAliases.justAdditionalAliases
 buildscript {
     repositories {
-        gradlePluginPortal()
         mavenCentral()
     }
     dependencies {
-        classpath("de.fayard.refreshVersions:refreshVersions:0.9.7")
-////                                             # available:0.10.0")
         classpath("org.danilopianini:refreshversions-aliases:+")
     }
 }
-migrateRefreshVersionsIfNeeded("0.9.7") // Will be automatically removed by refreshVersions when upgraded to the latest version.
-
-bootstrapRefreshVersions(justAdditionalAliases)
 
 plugins {
     id("com.gradle.enterprise") version "3.2"
+    id("de.fayard.refreshVersions") version "0.10.0"
+}
+
+refreshVersions {
+    extraArtifactVersionKeyRules = justAdditionalAliases
 }
 
 gradleEnterprise {
